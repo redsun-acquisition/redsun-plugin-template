5c0dbfe3527c695dba77f718dd5f4bd7a9eb479c
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-19 19:54:29 +0100

    Update context hook
    - add config file management
    - create context variables for `__init__.py`

--- extensions/context.py
+++ extensions/context.py
@@ -5,31 +5,46 @@ class ContextUpdater(ContextHook):
     def hook(self, context: dict) -> dict:
         """Add hook to update context based on user's input."""
         if context["plugin_type"] == "Model":
+            context["module_import"] = "model"
             if context["plugin_model_type"] == "Detector":
                 context = self._chose_detector(context)
             else: # motor model
                 context = self._chose_motor(context)
+        context = self._set_config_info(context)
         return context
     
     def _chose_detector(self, context: dict) -> dict:
         """Select which detector model base class to use."""
         if context["mm_support"]:
-            context["plugin_base"] == "ExEngineMMCameraModel"
+            context["plugin_base"] = "ExEngineMMCameraModel"
         else:
-            context["plugin_base"] == "ExEngineDetectorModel"
+            context["plugin_base"] = "ExEngineDetectorModel"
+        context["class_baseline"] = "MyDetectorModel"
         return context
     
     def _chose_motor(self, context: dict) -> dict:
         """Select which motor model base class to use."""
         if context["plugin_motor_type"] == "Single":
             if context["mm_support"]:
-                context["plugin_base"] == "ExEngineMMSingleMotorModel"
+                context["plugin_base"] = "ExEngineMMSingleMotorModel"
             else:
-                context["plugin_base"] == "ExEngineSingleMotorModel"
+                context["plugin_base"] = "ExEngineSingleMotorModel"
         else: # dual-axis (XY) motor
             if context["mm_support"]:
-                context["plugin_base"] == "ExEngineMMDoubleMotorModel"
+                context["plugin_base"] = "ExEngineMMDoubleMotorModel"
             else:
-                context["plugin_base"] == "ExEngineDoubleMotorModel"
+                context["plugin_base"] = "ExEngineDoubleMotorModel"
+        context["class_baseline"] = "MyMotorModel"
+        return context
+    
+    def _set_config_info(self, context: dict) -> dict:
+        """Set config_info and config_base based on plugin_type."""
+        if context["plugin_type"] == "Model":
+            if context["plugin_model_type"] == "Detector":
+                context["config_base"] = "DetectorModelInfo"
+                context["config_info"] = "MyDetectorInfo"
+            else: # motor model
+                context["config_base"] = "MotorModelInfo"
+                context["config_info"] = "MyMotorInfo"
         return context
     
