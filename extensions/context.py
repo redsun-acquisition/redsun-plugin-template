885683eb40be61d050dfc0b7482b7036de91a1b4
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-19 18:24:29 +0100

    Refactor for easier readability

--- extensions/context.py
+++ extensions/context.py
@@ -6,20 +6,30 @@ class ContextUpdater(ContextHook):
         """Add hook to update context based on user's input."""
         if context["plugin_type"] == "Model":
             if context["plugin_model_type"] == "Detector":
-                if context["mm_support"]:
-                    context["plugin_base"] == "ExEngineMMCameraModel"
-                else:
-                    context["plugin_base"] == "ExEngineDetectorModel"
+                context = self._chose_detector(context)
             else: # motor model
-                if context["plugin_motor_type"] == "Single":
-                    if context["mm_support"]:
-                        context["plugin_base"] == "ExEngineMMSingleMotorModel"
-                    else:
-                        context["plugin_base"] == "ExEngineSingleMotorModel"
-                else: # dual-axis (XY) motor
-                    if context["mm_support"]:
-                        context["plugin_base"] == "ExEngineMMDoubleMotorModel"
-                    else:
-                        context["plugin_base"] == "ExEngineDoubleMotorModel"       
+                context = self._chose_motor(context)
+        return context
+    
+    def _chose_detector(self, context: dict) -> dict:
+        """Select which detector model base class to use."""
+        if context["mm_support"]:
+            context["plugin_base"] == "ExEngineMMCameraModel"
+        else:
+            context["plugin_base"] == "ExEngineDetectorModel"
+        return context
+    
+    def _chose_motor(self, context: dict) -> dict:
+        """Select which motor model base class to use."""
+        if context["plugin_motor_type"] == "Single":
+            if context["mm_support"]:
+                context["plugin_base"] == "ExEngineMMSingleMotorModel"
+            else:
+                context["plugin_base"] == "ExEngineSingleMotorModel"
+        else: # dual-axis (XY) motor
+            if context["mm_support"]:
+                context["plugin_base"] == "ExEngineMMDoubleMotorModel"
+            else:
+                context["plugin_base"] == "ExEngineDoubleMotorModel"
         return context
     
