62d74fb3ce9c5a892ac97ee8df280cbee7472c1f
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-26 17:37:49 +0100

    Add more test
    - still need to test actual generated plugin

--- tests/test_create_template.py
+++ tests/test_create_template.py
@@ -73,6 +73,7 @@ def test_create_template_model_exengine(
 
 
 @pytest.mark.parametrize("plugin_model_type", ["Detector", "Motor"])
+@pytest.mark.filterwarnings("ignore::UserWarning")
 def test_create_template_model_bluesky(copie: "Copie", plugin_model_type: str) -> None:
     """Create a new plugin."""
 
@@ -112,6 +113,44 @@ def test_create_template_model_bluesky(copie: "Copie", plugin_model_type: str) -
         "src", "foo_bar", "engine", "bluesky", "model.py"
     ).is_file()
 
+@pytest.mark.parametrize("plugin_engine", ["ExEngine", "Bluesky"])
+@pytest.mark.filterwarnings("ignore::UserWarning")
+def test_create_controller(copie: "Copie", plugin_engine: str) -> None:
+    """Create a new plugin."""
+
+    answers = {
+        "full_name": "plugin bot",
+        "email": "yourmail@mail.com",
+        "plugin_name": "foo-bar",
+        "display_name": "Foo Bar",
+        "module_name": "foo_bar",
+        "short_description": "Super fast foo for all the bars",
+        "github_username_or_organization": "githubuser",
+        "github_repository_url": "provide later",
+        "plugin_engine": plugin_engine,
+        "plugin_type": "Controller",
+        "use_git_tags_for_versioning": False,
+        "install_precommit": False,
+        "license": "MIT",
+    }
+    result = copie.copy(extra_answers=answers)
+    
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_dir.is_dir()
+    with open(result.project_dir / "README.md") as f:
+        assert f.readline() == "# foo-bar\n"
+    assert result.project_dir.joinpath("src").is_dir()
+    assert result.project_dir.joinpath("src", "foo_bar", "__init__.py").is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "engine", plugin_engine, "__init__.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "engine", plugin_engine, "config.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "engine", plugin_engine, "controller.py"
+    ).is_file()
 
 # @pytest.mark.parametrize("include_reader_plugin", [True, False])
 # @pytest.mark.parametrize("include_writer_plugin", [True, False])
