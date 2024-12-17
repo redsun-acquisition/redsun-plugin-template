e10d22a899334664e6a742b176c17fd6e00b6a25
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-17 19:34:25 +0100

    Update template
    - add class_baseline as option

--- extensions/context.py
+++ extensions/context.py
@@ -6,57 +6,13 @@ class ContextUpdater(ContextHook):
         """Add hook to update context based on user's input."""
         if context["plugin_type"] == "Controller":
             context["module_import"] = "controller"
-            if context["plugin_engine"] == "ExEngine":
-                context["plugin_base"] = "ExEngineController"
-                context["registry"] = "ExEngineDeviceRegistry"
-            else: # Bluesky
-                context["plugin_base"] = "BlueskyController"
-                context["registry"] = "BlueskyDeviceRegistry"
+            context["plugin_base"] = "BlueskyController"
+            context["registry"] = "BlueskyDeviceRegistry"
             context["class_baseline"] = "MyController"
         else:
-            if context["plugin_engine"] == "ExEngine":
-                context = self._chose_exengine_context(context)
-            else: # Bluesky
-                context = self._chose_bluesky_context(context)        
-            context = self._set_config_info(context)
-        context["plugin_engine"] = context["plugin_engine"].lower()
-        return context
-    
-    ########################################################
-    ################ ExEngine context ######################
-    ########################################################
-    def _chose_exengine_context(self, context: dict) -> dict:
-        """Select which ExEngine model base class to use."""
-        if context["plugin_type"] == "Model":
-            context["module_import"] = "model"
-            if context["plugin_model_type"] == "Detector":
-                context = self._chose_exengine_detector(context)
-            else: # motor model
-                context = self._chose_exengine_motor(context)
-        return context
-    
-    def _chose_exengine_detector(self, context: dict) -> dict:
-        """Select which detector model base class to use."""
-        if context["mm_support"]:
-            context["plugin_base"] = "ExEngineMMCameraModel"
-        else:
-            context["plugin_base"] = "ExEngineDetectorModel"
-        context["class_baseline"] = "MyDetectorModel"
-        return context
-    
-    def _chose_exengine_motor(self, context: dict) -> dict:
-        """Select which motor model base class to use."""
-        if context["plugin_motor_type"] == "Single":
-            if context["mm_support"]:
-                context["plugin_base"] = "ExEngineMMSingleMotorModel"
-            else:
-                context["plugin_base"] = "ExEngineSingleMotorModel"
-        else: # dual-axis (XY) motor
-            if context["mm_support"]:
-                context["plugin_base"] = "ExEngineMMDoubleMotorModel"
-            else:
-                context["plugin_base"] = "ExEngineDoubleMotorModel"
-        context["class_baseline"] = "MyMotorModel"
+            context = self._chose_bluesky_context(context)        
+        context = self._set_config_info(context)
+        context["plugin_engine"] = "bluesky"
         return context
     
     def _set_config_info(self, context: dict) -> dict:
@@ -64,15 +20,11 @@ class ContextUpdater(ContextHook):
         if context["plugin_type"] == "Model":
             if context["plugin_model_type"] == "Detector":
                 context["config_base"] = "DetectorModelInfo"
-                context["config_info"] = "MyDetectorInfo"
             else: # motor model
                 context["config_base"] = "MotorModelInfo"
-                context["config_info"] = "MyMotorInfo"
+        context["config_info"] = context["class_baseline"].split("Model") + "Info"
         return context
-    
-    ########################################################
-    ################# Bluesky context ######################
-    ########################################################
+
     def _chose_bluesky_context(self, context: dict) -> dict:
         """Select which Bluesky model base class to use."""
         if context["plugin_type"] == "Model":
