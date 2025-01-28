6881bebb5dd6f74f231a6baec9ea157fc79f79f5
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-01-29 00:19:43 +0100

    Add support for widgets

--- extensions/context.py
+++ extensions/context.py
@@ -7,13 +7,18 @@ class ContextUpdater(ContextHook):
         """Add hook to update context based on user's input."""
         if context["plugin_type"] == "Controller":
             context["module_import"] = "controller"
-            context["class_baseline"] = "MyController"
             context["entry_point_group"] = "controllers"
-        else:
+            if context["add_widget"]:
+                context["widget_module_import"] = "widget"
+                context["widget_entry_point_group"] = "widgets"
+        elif context["plugin_type"] == "Model":
             context["module_import"] = "model"
-            context["class_baseline"] = "MyModel"
             context["entry_point_group"] = "models"
+        else:
+            context["module_import"] = "widget"
+            context["entry_point_group"] = "widgets"
         context["config_info"] = context["class_baseline"] + "Info" 
         context["entry_point_value"] = context["class_baseline"].lower()
         context["entry_point_cfg_value"] = context["class_baseline"].lower() + "_config"
+        
         return context
