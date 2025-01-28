64d165a4b6db83c0030bf9fd1914eacf43dde1da
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-01-28 22:35:47 +0100

    Add support for widget creation

--- tests/test_create_template.py
+++ tests/test_create_template.py
@@ -10,22 +10,22 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pytest_copie.plugin import Copie
 
-
-@pytest.mark.parametrize("plugin_model_type", ["Detector", "Motor"])
 @pytest.mark.filterwarnings("ignore::UserWarning")
-def test_create_template_model_bluesky(copie: Copie) -> None:
+def test_create_model(copie: Copie) -> None:
     """Create a new plugin."""
 
     answers = {
         "full_name": "plugin bot",
         "email": "yourmail@mail.com",
+        "github_username_or_organization": "githubuser",
         "plugin_name": "foo-bar",
         "display_name": "Foo Bar",
         "module_name": "foo_bar",
         "short_description": "Super fast foo for all the bars",
-        "github_username_or_organization": "githubuser",
         "github_repository_url": "provide later",
         "plugin_type": "Model",
+        "class_baseline": "MyBaseModel",
+        "add_widget": False,
         "install_precommit": False,
         "license": "MIT",
     }
@@ -41,13 +41,23 @@ def test_create_template_model_bluesky(copie: Copie) -> None:
     assert result.project_dir.joinpath("src").is_dir()
     assert result.project_dir.joinpath("src", "foo_bar", "__init__.py").is_file()
     assert result.project_dir.joinpath(
-        "src", "foo_bar", "engine", "bluesky", "__init__.py"
+        "src", "foo_bar", "__init__.py"
     ).is_file()
     assert result.project_dir.joinpath(
-        "src", "foo_bar", "engine", "bluesky", "config.py"
+        "src", "foo_bar", "config.py"
     ).is_file()
     assert result.project_dir.joinpath(
-        "src", "foo_bar", "engine", "bluesky", "model.py"
+        "src", "foo_bar", "model.py"
+    ).is_file()
+
+    # make sure a controller was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "controller.py"
+    ).is_file()
+
+    # make sure a widget was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "widget.py"
     ).is_file()
 
 
@@ -58,13 +68,15 @@ def test_create_controller(copie: Copie) -> None:
     answers = {
         "full_name": "plugin bot",
         "email": "yourmail@mail.com",
+        "github_username_or_organization": "githubuser",
         "plugin_name": "foo-bar",
         "display_name": "Foo Bar",
         "module_name": "foo_bar",
         "short_description": "Super fast foo for all the bars",
-        "github_username_or_organization": "githubuser",
         "github_repository_url": "provide later",
         "plugin_type": "Controller",
+        "class_baseline": "MyBaseController",
+        "add_widget": False,
         "install_precommit": False,
         "license": "MIT",
     }
@@ -86,4 +98,106 @@ def test_create_controller(copie: Copie) -> None:
     ).is_file()
     assert result.project_dir.joinpath(
         "src", "foo_bar", "controller.py"
+    ).is_file()
+
+    # make sure a model was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "model.py"
+    ).is_file()
+
+    # make sure a widget was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "widget.py"
+    ).is_file()
+
+@pytest.mark.filterwarnings("ignore::UserWarning")
+def test_create_widget(copie: Copie) -> None:
+    answers = {
+        "full_name": "plugin bot",
+        "email": "yourmail@mail.com",
+        "github_username_or_organization": "githubuser",
+        "plugin_name": "foo-bar",
+        "display_name": "Foo Bar",
+        "module_name": "foo_bar",
+        "short_description": "Super fast foo for all the bars",
+        "github_repository_url": "provide later",
+        "plugin_type": "Widget",
+        "class_baseline": "MyBaseWidget",
+        "install_precommit": False,
+        "license": "MIT",
+    }
+    result = copie.copy(extra_answers=answers)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_dir is not None
+    assert result.project_dir.is_dir()
+    with open(result.project_dir / "README.md") as f:
+        assert f.readline() == "# foo-bar\n"
+    assert result.project_dir.joinpath("src").is_dir()
+    assert result.project_dir.joinpath("src", "foo_bar", "__init__.py").is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "__init__.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "config.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "widget.py"
+    ).is_file()
+
+    # make sure a model was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "model.py"
+    ).is_file()
+
+    # make sure a controller was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "controller.py"
+    ).is_file()
+
+@pytest.mark.filterwarnings("ignore::UserWarning")
+def test_create_model_controller_widget(copie: Copie) -> None:
+    answers = {
+        "full_name": "plugin bot",
+        "email": "yourmail@mail.com",
+        "github_username_or_organization": "githubuser",
+        "plugin_name": "foo-bar",
+        "display_name": "Foo Bar",
+        "module_name": "foo_bar",
+        "short_description": "Super fast foo for all the bars",
+        "github_repository_url": "provide later",
+        "plugin_type": "Controller",
+        "class_baseline": "MyBaseController",
+        "add_widget": True,
+        "widget_class_baseline": "MyBaseWidget",
+        "install_precommit": False,
+        "license": "MIT",
+    }
+    result = copie.copy(extra_answers=answers)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_dir is not None
+    assert result.project_dir.is_dir()
+    with open(result.project_dir / "README.md") as f:
+        assert f.readline() == "# foo-bar\n"
+    assert result.project_dir.joinpath("src").is_dir()
+    assert result.project_dir.joinpath("src", "foo_bar", "__init__.py").is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "__init__.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "config.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "controller.py"
+    ).is_file()
+    assert result.project_dir.joinpath(
+        "src", "foo_bar", "widget.py"
+    ).is_file()
+
+    # make sure a model was not generated
+    assert not result.project_dir.joinpath(
+        "src", "foo_bar", "model.py"
     ).is_file()
